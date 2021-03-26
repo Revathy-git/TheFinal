a3695f47d93550a59a030dc5fe3f15051b5fee72
Revathy-git <revathye28@gmail.com>
2021-03-25 22:37:58 -0700

    api

---
+++ api/queries.js
@@ -0,0 +1,74 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'genesys',
+    host: '127.0.0.1',
+    database: 'api',
+    password: 'genesys',
+    port: 5432,
+})
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getUserById = (request, response) => {
+    const id = request.params.firstname
+
+    pool.query('SELECT * FROM users WHERE firstname = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const createUser = (request, response) => {
+    console.log(request.body.data);
+    const { firstName, lastName, email, role } = request.body.data
+
+    pool.query('INSERT INTO users (firstname, lastname, email, role) VALUES ($1, $2, $3, $4)', [firstName, lastName, email, role], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`User added`)
+    })
+}
+
+const updateUser = (request, response) => {
+    const id = request.params.firstname
+    const { email } = request.body
+
+    pool.query(
+        'UPDATE users SET email = $1 WHERE firstname = $2',
+        [email, id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`User modified`)
+        }
+    )
+}
+
+const deleteUser = (request, response) => {
+    const id = request.params.firstname
+
+    pool.query('DELETE FROM users WHERE firstname = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User deleted`)
+    })
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+}
---
+++ api/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3080
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'genesys',
+    host: '127.0.0.1',
+    database: 'api',
+    password: 'genesys',
+    port: 5432,
+})
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:firstname', db.getUserById)
+app.post('/api/users', db.createUser)
+app.put('/users/:firstname', db.updateUser)
+app.delete('/users/:firstname', db.deleteUser)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
