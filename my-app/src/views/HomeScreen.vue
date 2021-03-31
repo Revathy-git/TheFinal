0839d493267f10f02e084d5cc489abf3b8a12ef8
Revathy-git <revathye28@gmail.com>
2021-03-30 23:26:27 -0700

    leaderboard changed to localhost

--- my-app/src/views/HomeScreen.vue
+++ my-app/src/views/HomeScreen.vue
@@ -11,7 +11,7 @@
      <ul class="sidebar-panel-nav">
        <button type="button" @click='clickHome()' class="btn btn-danger">Home</button><br><br>
        <a href="http://localhost:3000/calendar" tag="li" class="btn btn-danger">Calendar</a><br><br>
-       <a href="http://172.24.135.111:8082/leaderboard.html" tag="li" class="btn btn-danger">LeaderBoard</a><br><br>
+       <a href="http://localhost:8082/leaderboard.html" tag="li" class="btn btn-danger">LeaderBoard</a><br><br>
        <button type="button" @click='clickMenteeView()' class="btn btn-danger">Mentee View</button><br><br>
        <button type="button" @click='clickMentorView()' class="btn btn-danger">Mentor View</button><br><br>
        <button type="button" @click='clickProfile()' class="btn btn-danger">Profile</button><br><br>
